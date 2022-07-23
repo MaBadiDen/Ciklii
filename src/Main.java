969ca5e6da60652b025a3f0e7d8ec1ced4024fb9
MaBadiDen <kaba4ok.den@gmail.com>
2022-07-23 13:50:15 +0300

    Готово

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        /*Первое задание*/
+        int i = 1;
+
+        while (i <= 10){
+            System.out.print(i + " ");
+            i += 1;
+        }
+        System.out.println();
+        for(int j = 10; j >= 1; j--)
+        {
+            System.out.print(j + " ");
+        }
+        System.out.println();
+
+        /*Второе задание*/
+        int firstFriday = 4;
+
+        for(; firstFriday <= 31; firstFriday += 7)
+        {
+            System.out.println("Сегодня пятница, " + firstFriday + "-е число, за работу, бездельник, нужен отчет!");
+        }
+
+        /*Третье задание*/
+        int yearNow = 2022;
+        int year = yearNow - 200;
+        while (year <= (yearNow + 100)) {
+            if (year % 79 == 0) {
+                System.out.println(year);
+            }
+            year++;
+        }
     }
 }
